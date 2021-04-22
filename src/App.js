f7f4e798cee6a9f2f60449173dc601dd7dae9d3e
alineolic <72208027+alineolic@users.noreply.github.com>
2021-04-22 23:13:14 +0100

    Updated link to GitHub repo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
 <Weather defaultCity="London" />
 <br />
         <footer>
-          This project was coded by <a href="https://www.alineclaudio.com" target="_blank" rel="noreferrer"> Aline Claudio</a> and is open-sourced on <a href="https://github.com/alineolic/new-react-weather-app" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://app.netlify.com/" target="_blank" rel="noreferrer">Netlify</a>.
+          This project was coded by <a href="https://www.alineclaudio.com" target="_blank" rel="noreferrer"> Aline Claudio</a> and is open-sourced on <a href="https://github.com/alineolic/react-weather-app-new2" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://app.netlify.com/" target="_blank" rel="noreferrer">Netlify</a>.
         </footer>
 </div>
     </div>
